932459f233cf08dc7be32f7c8bdaa42ac0944ece
Patrick Fu <paaatrickfu@gmail.com>
2022-09-30 19:28:45 +0800

    🐛 Fix the native cmake config, collect all source files

--- native/android/zego_express_engine_plugin-config.cmake
+++ native/android/zego_express_engine_plugin-config.cmake
@@ -1,7 +1,9 @@
 set(ZEGO_EXPRESS_LIBRARY_PATH "${CMAKE_CURRENT_LIST_DIR}/libs/${ANDROID_ABI}")
 message(STATUS "[ZEGO] Express library path: ${ZEGO_EXPRESS_LIBRARY_PATH}")
 
-aux_source_directory(${CMAKE_CURRENT_LIST_DIR}/../src SRC_LIST)
+set(SRC_DIR ${CMAKE_CURRENT_LIST_DIR}/../src)
+file(GLOB_RECURSE SRC_LIST ${SRC_DIR}/*.h ${SRC_DIR}/*.cc)
+source_group(TREE ${SRC_DIR} PREFIX "src" FILES ${SRC_LIST})
 add_library(zego_express_engine_plugin ${SRC_LIST})
 
 target_include_directories(zego_express_engine_plugin PRIVATE "${ZEGO_EXPRESS_LIBRARY_PATH}/include")
--- native/windows/zego_express_engine_plugin-config.cmake
+++ native/windows/zego_express_engine_plugin-config.cmake
@@ -7,7 +7,9 @@ endif()
 set(ZEGO_EXPRESS_LIBRARY_PATH "${CMAKE_CURRENT_LIST_DIR}/libs/${WIN_ARCH}")
 message(STATUS "[ZEGO] Express library path: ${ZEGO_EXPRESS_LIBRARY_PATH}")
 
-aux_source_directory(${CMAKE_CURRENT_LIST_DIR}/../src SRC_LIST)
+set(SRC_DIR ${CMAKE_CURRENT_LIST_DIR}/../src)
+file(GLOB_RECURSE SRC_LIST ${SRC_DIR}/*.h ${SRC_DIR}/*.cc)
+source_group(TREE ${SRC_DIR} PREFIX "src" FILES ${SRC_LIST})
 add_library(zego_express_engine_plugin ${SRC_LIST})
 
 target_include_directories(zego_express_engine_plugin PRIVATE "${ZEGO_EXPRESS_LIBRARY_PATH}/include")
