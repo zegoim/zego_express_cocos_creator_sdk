dee829c533b3c4a88beec51e2f6fa5fe2825b153
Patrick Fu <paaatrickfu@gmail.com>
2023-01-16 23:54:16 +0800

    🐛 Fix a crash when exiting the game directly during streaming

--- native/src/internal/zego_utils.cc
+++ native/src/internal/zego_utils.cc
@@ -19,7 +19,27 @@ uint32_t GetNextSequence() {
 }
 
 void RunOnCocosThread(const std::function<void()> &func) {
-    CC_CURRENT_ENGINE()->getScheduler()->performFunctionInCocosThread(func);
+    auto cc_manager = CC_APPLICATION_MANAGER();
+    if (!cc_manager) {
+        CC_LOG_WARNING("[ZEGO] cc application manager is null!");
+        return;
+    }
+    auto cc_current_application = cc_manager->getCurrentApp();
+    if (!cc_current_application) {
+        CC_LOG_WARNING("[ZEGO] cc current application is null!");
+        return;
+    }
+    auto cc_engine = cc_current_application->getEngine();
+    if (!cc_engine) {
+        CC_LOG_WARNING("[ZEGO] cc engine is null!");
+        return;
+    }
+    auto cc_scheduler = cc_engine->getScheduler();
+    if (!cc_scheduler) {
+        CC_LOG_WARNING("[ZEGO] cc scheduler is null!");
+        return;
+    }
+    cc_scheduler->performFunctionInCocosThread(func);
 }
 
 } // namespace zego::cocos
